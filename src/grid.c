0b4e4f5043e0a785159e72ca18092de96b817b47
Ilya Savitsky <ipsavitsky234@gmail.com>
2023-09-12 22:24:30 +0100

    Removed documentation; optimized an if

--- src/grid.c
+++ src/grid.c
@@ -3,14 +3,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-/**
- * @brief Allocate memory for the grid, Set Width and Height. Each cell is allovated according to prob
- *
- * @param gr Grid pointer
- * @param width Width of the grid
- * @param height Height of the grid
- * @param prob Probability of a cell being alive
- */
 void grid_init_random(grid **gr, uint width, uint height, float prob) {
     *gr = (grid *)malloc(sizeof(grid));
     (*gr)->width = width;
@@ -22,48 +14,19 @@ void grid_init_random(grid **gr, uint width, uint height, float prob) {
     }
 }
 
-/**
- * @brief Get the state of a cell in grid gr at coordinates i, j
- *
- * @param gr Grid to get coords out of
- * @param i x coordinate
- * @param j y coordinate
- * @return CellState State of the cell at i, j in grid
- */
 CellState get(grid *gr, uint i, uint j) {
     return gr->field[i * gr->height + j];
 }
 
-/**
- * @brief Set the state of a cell in grid at coordinates i, j
- *
- * @param gr Grid to get coords out of
- * @param i x coordinate
- * @param j y coordinate
- * @param val CellState to set cell to
- */
 void set(grid *gr, uint i, uint j, CellState val) {
     gr->field[i * gr->height + j] = val;
 }
 
-/**
- * @brief Free the memory allocated for grid gr.
- *
- * @param gr grid to free
- */
 void grid_free(grid *gr) {
     free(gr->field);
     free(gr);
 }
 
-/**
- * @brief Calculate amount of alive neighbors of a cell in grid C
- *
- * @param gr Grid to calculate neighbors in
- * @param i x coordinate
- * @param j y coordinate
- * @return uint Amount of neighbors of point at coordinates i, j
- */
 uint grid_calc_neighbors(grid *gr, uint i, uint j) {
     uint count = 0;
     for (long i_cor = i - 1; i_cor <= i + 1; i_cor++) {
@@ -78,11 +41,6 @@ uint grid_calc_neighbors(grid *gr, uint i, uint j) {
     return count;
 }
 
-/**
- * @brief Progress Game Of Life
- *
- * @param gr Grid to have game processed in
- */
 void grid_run_gol_step(grid *gr) {
     CellState *field_copy =
         (CellState *)malloc(sizeof(CellState) * gr->width * gr->height);
@@ -90,8 +48,8 @@ void grid_run_gol_step(grid *gr) {
     for (uint i = 0; i < gr->width; ++i) {
         for (uint j = 0; j < gr->height; ++j) {
             uint neigh = grid_calc_neighbors(gr, i, j);
-            if ((alive == get(gr, i, j) && (2 == neigh || 3 == neigh)) ||
-                (dead == get(gr, i, j) && 3 == neigh)) {
+            if ((3 == neigh) ||
+                (2 == neigh && alive == get(gr, i, j))) {
                 field_copy[i * gr->height + j] = alive;
             } else {
                 field_copy[i * gr->height + j] = dead;
